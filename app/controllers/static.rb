b4278b763a661e56bde00b3ab9a54cb4784181ea
sugiarto <ugidmtest@gmail.com>
2018-08-26 13:38:58 +0700

    support api

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -20,9 +20,23 @@ end
 
 
 get '/:short_url' do
-	@url = Url.find_by(short_url: params[:short_url])
-	@url.click_count += 1
-	@url.save
-	
-	redirect "#{@url.long_url}"
+  @url = Url.find_by(short_url: params[:short_url])
+  if @url
+    @url.click_count += 1
+    @url.save	
+    redirect "#{@url.long_url}"
+  else
+    redirect "/"
+  end
+end
+
+post '/api_url' do
+  content_type :json 
+  @url = Url.new(long_url: params[:long_url]) 
+ 
+  if @url.save
+    {'status' => 'ok', 'url' => "http://s.snppt.com/#{@url.short_url}"}.to_json
+  else
+    {'status' => 'error'}.to_json
+  end
 end
